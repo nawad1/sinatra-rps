c38cc5880a5c2adae239637f2a972537326dbe81
nawad1 <nawad1@chicagobooth.edu>
2024-07-05 02:19:44 +0000

    Basic structure

--- app.rb
+++ app.rb
@@ -7,3 +7,48 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("/rock") do
+  moves = ["rock","paper","scissors"]
+  @comp_move = moves.sample
+
+if @comp_move == "rock"
+  @outcome = "tied"
+elsif @comp_move == "paper"
+  @outcome = "lost"
+else 
+  @outcome = "won"
+end
+
+erb(:rock)
+end
+
+get("/paper") do
+  moves = ["rock","paper","scissors"]
+  @comp_move = moves.sample
+
+if @comp_move == "rock"
+  @outcome = "won"
+elsif @comp_move == "paper"
+  @outcome = "tied"
+else 
+  @outcome = "lost"
+end
+
+erb(:paper)
+end
+
+get("/scissors") do
+  moves = ["rock","paper","scissors"]
+  @comp_move = moves.sample
+
+if @comp_move == "rock"
+  @outcome = "lost"
+elsif @comp_move == "paper"
+  @outcome = "won"
+else 
+  @outcome = "tied"
+end
+
+erb(:scissors)
+end
